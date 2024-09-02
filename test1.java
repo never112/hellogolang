46292a7e5570c7fe587a5ccc80fc826b253539d6
zhouxiaoliang <leonzxl@163.com>
2024-09-02 18:18:59 +0800

    Update test1.java

--- test1.java
+++ test1.java
@@ -4,7 +4,7 @@ public class Student {
     String name;
     int age;
 
-  public void aazzddeefatgdf() {
+  public void aazzddeefatdgdf() {
       String test="12312";
         System.out.println("吃饭");
     }
