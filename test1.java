c81fccc6f09650aec6b48d56228e7ecf081b7fc5
wwcchh0123 <66157064+wwcchh0123@users.noreply.github.com>
2024-09-03 19:25:59 +0800

    Update test1.java

--- test1.java
+++ test1.java
@@ -4,7 +4,7 @@ public class Student {
     String name;
     int age;
 
-  public void aazzddeefadtdf() {
+  public void aazzddeefdddddddadtdf() {
       String test="12312";
         System.out.println("吃饭");
     }
